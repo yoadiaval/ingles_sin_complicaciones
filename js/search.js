4b576cec07ac3dbfb10bef85c0df29b4c460eaec
yoannet93 <yoannet93@gmail.com>
2023-05-13 21:24:38 +0200

    new save

--- js/search.js
+++ js/search.js
@@ -1,10 +1,9 @@
 document.addEventListener("keyup", e=>{
   if(e.target.matches("#buscador")){
    document.querySelectorAll(".h2_entry").forEach( item =>{
-    item.textContent.toLocaleLowerCase().includes(e.target.value)
+    item.textContent.toLocaleLowerCase().includes(e.target.value)//esta l'inea s la que compara 
     ? item.parentElement.parentElement.classList.remove("filtro")
     : item.parentElement.parentElement.classList.add("filtro")
-
    })
   }
 })
