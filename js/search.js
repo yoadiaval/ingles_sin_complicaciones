c81c5bdd25ebc386e2555e902f16f959582c9f91
yoannet93 <yoannet93@gmail.com>
2023-05-14 14:50:39 +0200

    modified js for search, dont work if input entry had uppercaseyou entry a caracter with uppercase

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 document.addEventListener("keyup", e=>{
   if(e.target.matches("#buscador")){
    document.querySelectorAll(".h2_entry").forEach( item =>{
-    item.textContent.toLocaleLowerCase().includes(e.target.value)//esta l'inea s la que compara 
+    item.textContent.toLowerCase().includes(e.target.value.toLowerCase())//esta l'inea s la que compara 
     ? item.parentElement.parentElement.classList.remove("filtro")
     : item.parentElement.parentElement.classList.add("filtro")
    })
